20f388626fc22bd55de58426573e2a3c7a1fbfcf
carlos8788 <carlos8788@msn.com>
2023-08-29 06:02:48 -0300

    useMemo ready

--- src/components/CalculosPesados.jsx
+++ src/components/CalculosPesados.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
-const getCalculo = (listaNumber) => {
-    console.log('calculando');
-    return listaNumber.reduce((a, b) => a * b * b)
-}
+
 
 export const CalculosPesados = () => {
-    const listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [listaNumber, setListaNumber] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
     const [show, setShow] = useState(true)
+
+    const agregarNumbers = () => {
+        setListaNumber([...listaNumber, listaNumber[listaNumber.length - 1] + 1])
+        console.log(listaNumber);
+    }
+
+    const getCalculo = (listaNumber) => useMemo(()=> {
+        console.log('calculando');
+        return listaNumber.reduce((a, b) => a * b * b)
+    }, [listaNumber])
 
     return (
         <>
-            <div>CalculosPesados</div>
-            <p>{getCalculo(listaNumeros)}</p>
-            <button className="btn btn-primary" onClick={()=> setShow(!show)} >{show ? 'Show' : 'Hide'}</button>
+            <div>Calculos Pesados</div>
+            <p>{getCalculo(listaNumber)}</p>
+            <button className="btn btn-primary me-2" onClick={()=> setShow(!show)} >{show ? 'Show' : 'Hide'}</button>
+            <button className="btn btn-info ms-2" onClick={agregarNumbers} >agregar numero</button>
         </>
     )
 }
